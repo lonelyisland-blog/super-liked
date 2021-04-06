3c89dcc5a4455862e10d5b7e9fea53f7fbcb4ddb
guanyun-helo <guanyun.helo@gmail.com>
2021-04-06 22:33:25 +0800

    fix: change like list

--- src/components/latestSuperLike.jsx
+++ src/components/latestSuperLike.jsx
@@ -38,7 +38,7 @@ function List() {
 
 
   const loadMore = () => {
-    setIndex(index + 5)
+    setIndex(index + 10)
   }
 
   const toUrl = (url) => {
@@ -48,7 +48,7 @@ function List() {
   }
 
   const getMoreContent = () => {
-    let temList = likeList.slice(index, index + 5)
+    let temList = likeList.slice(index, index + 10)
     let infoList = []
     const fn = (info) => {
       return Api.getSuperLikeInfo(info)
